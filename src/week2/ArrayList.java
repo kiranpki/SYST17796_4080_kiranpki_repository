94927ddec6f9034a5e3dbe7b1099dee2126175fa
kiranpki <59941698+kiranpki@users.noreply.github.com>
2020-01-27 12:11:20 -0500

    Update ArrayList.java

--- src/week2/ArrayList.java
+++ src/week2/ArrayList.java
@@ -1,7 +1,7 @@
 package week2;
 public class ArrayList {
   public static void main(String[] args) {
-    String[] cars = {"Kiran","Roop","Gurtej","Nasreen"};
+    String[] cars = {"Kiran","Roop"};
       for (String car : cars) {
           System.out.println(car);
       }
