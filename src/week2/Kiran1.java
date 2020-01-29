123048d1da969945ae80311b67a090bf1b0e2b01
kiranpki <kiranpki@sheridancollege.ca>
2020-01-29 17:27:41 -0500

    new object

---
+++ src/week2/Kiran1.java
@@ -0,0 +1,14 @@
+package week2;
+public class Kiran1 {
+        private String _balance;
+   
+    public String getBalance() {
+        String _balance = null;
+        return _balance;
+    }
+
+    public void setBalance(String _balance) {
+        this._balance = _balance;
+   }
+    
+}
