b2338cdbd61b5f1b82bb05febde8f30793a9b274
kiranpki <kiranpki@sheridancollege.ca>
2020-01-29 12:24:53 -0500

    Merge pull request #1 from kiranpki/kiranpki-patch-1
    Update Arrayobject.java
     new java program

--- src/week2/Arrayobject.java
+++ src/week2/Arrayobject.java
@@ -2,12 +2,10 @@ package week2;
 public class Arrayobject {
 	public static void main (String args[ ])
 	{
-		String myArray[ ] = new String [4];
+		String myArray[ ] = new String [1];
 		//loading the array
 		myArray[0] = "Software design";
-		myArray[1] = "User design";
-		myArray[2] = "Java-2";
-                myArray[3] = "Linux/Unix";
+		
  		System.out.println("List of programs");
                 for (String myArray1 : myArray) {
                 System.out.println(myArray1);
