7aa9b6e910d8b480a43868ea591f3e9238c54dc2
kiranpki <kiranpki@sheridancollege.ca>
2020-01-29 13:52:00 -0500

    program object

---
+++ src/Array.java
@@ -0,0 +1,13 @@
+
+public class Array {
+    private int _number;
+
+    public int getNumber() {
+        return _number;
+    }
+
+    public void setNumber(int _number) {
+        this._number = _number;
+    }
+    
+}
