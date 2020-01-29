21b352b5e3514a9d4f201f30fe8545cf7ae8ce6d
kiranpki <59941698+kiranpki@users.noreply.github.com>
2020-01-29 16:15:57 -0500

    String deleted

--- src/week2/ArrayList.java
+++ src/week2/ArrayList.java
@@ -4,11 +4,9 @@ import java.util.Arrays;
 
 public class ArrayList {
   public static void main(String[] args) {
-    String[] cars = {"komal","kiran"};
-    String[] cars1 = {"komal","kiran"};
+    String[] cars = {"kiran"};
       for (String car : cars) {
           System.out.println(car);
       }
-      System.out.println(Arrays.toString(cars1));
   }
 }
