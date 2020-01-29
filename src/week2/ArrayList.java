65f2fc52e4a78d3300713551f72e18bd3e052330
kiranpki <kiranpki@sheridancollege.ca>
2020-01-29 16:11:57 -0500

    Array

--- src/week2/ArrayList.java
+++ src/week2/ArrayList.java
@@ -1,9 +1,14 @@
 package week2;
+
+import java.util.Arrays;
+
 public class ArrayList {
   public static void main(String[] args) {
     String[] cars = {"komal","kiran"};
+    String[] cars1 = {"komal","kiran"};
       for (String car : cars) {
           System.out.println(car);
       }
+      System.out.println(Arrays.toString(cars1));
   }
 }
